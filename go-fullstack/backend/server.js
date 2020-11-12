043d459c4d5b44314381d163e43bfc60b6e404f5
moueza <>
2020-11-12 05:48:06 +0100

    Resolves #7 p1c3part3 Démarrez un serveur basique. See #1 gen : local server OK

---
+++ go-fullstack/backend/server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.end('Voilà la réponse du serveur !');
+});
+
+server.listen(process.env.PORT || 3000);
+//log.error
+console.log('server running');
