183e3705c8fe0fd5e4e920397be064e5726032ed
moueza <>
2020-11-12 06:04:51 +0100

    Resolves #8 Installez nodemon. See #1 gen : local server OK

--- go-fullstack/backend/server.js
+++ go-fullstack/backend/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.end('Voilà la réponse du serveur !');
+    res.end('Voilà la réponse du serveur by moueza!');
 });
 
 server.listen(process.env.PORT || 3000);
