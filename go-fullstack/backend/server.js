20c8493545c7aa7dcc01d2511bee918d0040a27b
moueza <>
2020-12-09 07:16:26 +0100

    Resolves #13 p1c4part2 Executez l application Express sur le serveur Node . Resolves #14 p1c4part3 Ajoutez des middleware. see #1 gen OK

--- go-fullstack/backend/server.js
+++ go-fullstack/backend/server.js
@@ -1,9 +1,14 @@
 const http = require('http');
+const app = require('./app');
 
-const server = http.createServer((req, res) => {
-    res.end('Voilà la réponse du serveur by moueza!');
-});
+
+
+
+app.set('port', process.env.PORT || 3000);
+
+
+const server = http.createServer(app);
+
+
 
 server.listen(process.env.PORT || 3000);
-//log.error
-console.log('server running');
